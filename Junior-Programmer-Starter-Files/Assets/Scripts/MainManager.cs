4a233a4e2c8172e7a22ee617bbf4d4c095c09476
Elaine Sajets <elainesajets@gmail.com>
2025-10-16 18:21:33 +0900

    add new excercise about version control and data persistency

---
+++ Junior-Programmer-Starter-Files/Assets/Scripts/MainManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        File.WriteAllText(path, json);
+        Debug.Log($"[SaveColor] Saved to: {path}\n{json}");
+
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+            Debug.Log($"[LoadColor] Loaded from: {path}\n{json}");
+
+        }
+    }
+}
